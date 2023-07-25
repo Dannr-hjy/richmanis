3a8fcb8ca7e8ba27fbec41240df9761f599e4d39
Dannr <63134885+Dannr-hjy@users.noreply.github.com>
2023-07-25 19:28:46 +0800

    Add files via upload

---
+++ script/topbar.js
@@ -0,0 +1,30 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #e3f2fd;">
+<div class="container">
+ <a class="navbar-brand p-0 me-0 me-lg-2" href="/" aria-label="引导程序">
+     <img src="logo.svg" width="75" height="45" alt="logo">
+ </a>
+ <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="col align-self-center collapse navbar-collapse" id="navbarColor02">
+   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+     <li class="nav-item">
+       <a class="nav-link" href="#">主页</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">下载</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">论坛</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">关于</a>
+     </li>
+   </ul>
+ </div>
+</div>
+</nav>
+
+
+`);
